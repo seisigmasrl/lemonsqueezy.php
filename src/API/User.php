a2772a0a4f70adf87d49b680ef2c01de2e3abfcc
Ricardo Vargas <im@ricardovargas.me>
2023-04-03 04:49:57 -0400

    [REFACTOR] Reducing unnecessary calls.

--- src/API/User.php
+++ src/API/User.php
@@ -5,20 +5,26 @@ declare(strict_types=1);
 namespace LemonSqueezy\API;
 
 use LemonSqueezy\Entity\User as UserEntity;
+use stdClass;
 
 class User extends AbstractApi
 {
+    private stdClass $userData;
+
+    private function getUserData(): stdClass
+    {
+        return $this->userData = $this->get('/users/me');
+    }
+
     public function getUserId(): int
     {
-        $account = $this->get('/users/me');
-
+        $account = $this->userData ?? $this->getUserData();
         return (int) $account->data->id;
     }
 
     public function getUserInformation(): UserEntity
     {
-        $account = $this->get('/users/me');
-
+        $account = $this->userData ?? $this->getUserData();
         return new UserEntity($account->data->attributes);
     }
 }
